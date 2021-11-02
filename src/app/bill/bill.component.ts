8029f1ddccd7956417db210cf37494f9ffb99e8c
Jonathan Snead <nathan.snead1@gmail.com>
2021-11-01 21:28:43 -0500

    Updated styles Updated the page styles for each page. Added borders to tables and added ways to add more text. Added background image to all pages and it moves with page view.

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -10,10 +10,27 @@ export class BillComponent implements OnInit {
 
   // WILL BE REPLACED BY DATA FROM SPRING
   bill : Bill = {
-
-    cost : 100
-
+    phoneNumber: 10,
+    planNumber: 1000,
+    planCost : 100
   }
+  bill2 : Bill = {
+    phoneNumber: 10,
+    planNumber: 1000,
+    planCost : 100
+  }
+  bill3 : Bill = {
+    phoneNumber: 10,
+    planNumber: 1000,
+    planCost : 100
+  }
+  bill4 : Bill = {
+    phoneNumber: 10,
+    planNumber: 1000,
+    planCost : 100
+  }
+
+  //totalCost = planCost
   
   constructor() { }
 
